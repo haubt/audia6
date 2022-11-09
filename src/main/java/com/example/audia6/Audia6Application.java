eee874e936871ed57133a30cc4624be4fa042d54
Thomas H <Hauber.Thomas95@outlook.com>
2022-11-09 19:13:25 +0100

    Initial commit

---
+++ src/main/java/com/example/audia6/Audia6Application.java
@@ -0,0 +1,13 @@
+package com.example.audia6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Audia6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Audia6Application.class, args);
+    }
+
+}
